976bcaa65cf2677983ba55105b850653986a1ed6
Emma Zimmermann <ez019@hdm-stuttgart.de>
2024-05-25 16:01:32 +0200

    send post request for generate survey

--- frontend/src/Components/PollHeader/PollHeader.tsx
+++ frontend/src/Components/PollHeader/PollHeader.tsx
@@ -2,21 +2,35 @@ import React from 'react';
 import InputField from '../InputField';
 import c from './PollHeader.module.scss';
 
-const PollHeader = () => {
+interface PollHeaderProps {
+   onChangeHeading?: (value: string) => void;
+   onChangeDescription?: (value: string) => void;
+ }
+
+const PollHeader: React.FC<PollHeaderProps> = ({ onChangeHeading, onChangeDescription }) => {
+
+   const handleHeadingChange = (value: string) => {
+      if (onChangeHeading) {
+         onChangeHeading(value);
+      }
+    };
+
+    const handleDescriptionChange = (value: string) => {
+      if (onChangeDescription) {
+         onChangeDescription(value);
+      }
+    };
+
     return (
        <div>
          <p className={c.heading}>
             1. Select A Fancy Name For Your Poll
          </p>
-         <InputField label={'Heading'} placeholder={'Name of your poll'} onChange={function (value: string): void {
-            //TODO: Save Inputs
-         } } />
+         <InputField label={'Heading'} placeholder={'Name of your poll'} onChange={handleHeadingChange} />
          <p className={c.heading}>
             2. Write A Nice Description
          </p>
-         <InputField label={'Description'} placeholder={'This poll is about...'} onChange={function (value: string): void {
-            //TODO: Save Inputs
-         } } />
+         <InputField label={'Description'} placeholder={'This poll is about...'} onChange={handleDescriptionChange} />
          <p className={c.heading}>
             3. Check The Poll
          </p>
--- frontend/src/pages/WeddingTemplate.tsx
+++ frontend/src/pages/WeddingTemplate.tsx
@@ -5,9 +5,10 @@ import PageHeader from "../Components/PageHeader/PageHeader";
 import PollHeader from "../Components/PollHeader/PollHeader";
 import RangeSelector from "../Components/RangeSelector";
 import './template.scss';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 const WeddingTemplate = () => {
+
     useEffect(() => {
         const token = localStorage.getItem('token');
         if (!token) {
@@ -31,11 +32,40 @@ const WeddingTemplate = () => {
 
         checkToken();
     }, []);
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const pollType = "wedding";
+    const token = localStorage.getItem('token');
+
+
+    const handleHeadingChange = (value: string) => {
+        setTitle(value);
+    };
+
+    const handleDescriptionChange = (value: string) => {
+        setDescription(value);
+    };
+
+    const handleGeneratePoll = async () => {
+        try {
+        const response = await fetch('http://localhost:3001/polls', {
+            method: 'POST',
+            headers: {
+                Authorization: `${token}`
+            },
+            body: JSON.stringify({ description, pollType, title })
+        });
+        } catch (error) {
+        console.error('Error occurred during generate poll:', error);
+        }
+    };
+    
     return (
         <>
             <PageHeader heading="Create Wedding Poll" link="/select-template"/>
             <div className='template'>
-                <PollHeader />
+                <PollHeader onChangeHeading={handleHeadingChange} onChangeDescription={handleDescriptionChange} />
                 <p className='question'>Who invited you to the wedding?</p>
                 <MultipleChoiceSelector options={['Bride', 'Groom', 'Both']} onChange={function (option: string): void {} } />
                 <p className='question'>How long have you known the bride and groom?</p>
@@ -50,7 +80,7 @@ const WeddingTemplate = () => {
                     4. Everything Correct? Then Generate Your Poll!
                 </p>
                 <div className='generateButton'>
-                    <GenerateButton label={""} onClick={function (): void {} } />
+                    <GenerateButton label={""} onClick={handleGeneratePoll} />
                 </div>
             </div>
         </>
