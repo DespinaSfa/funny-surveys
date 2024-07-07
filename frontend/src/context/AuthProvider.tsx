6f07aad624330af6f5209408ec69908b6132fac4
mb389 <mb389@hdm-stuttgart.de>
2024-07-07 15:15:02 +0200

    Add checking for refresh token in AuthProvider.tsx

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -10,7 +10,37 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 interface AuthProviderProps {
     children: ReactNode;
 }
+const CheckRefreshToken = async () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (!refreshToken) {
+        console.log('No refresh token found');
+        return;
+    }
 
+    try {
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/refresh-token`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ refreshToken })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem('token', data.token);
+            localStorage.setItem('refreshToken', data.refreshToken);
+            console.log('Token refreshed');
+        } else {
+            console.error('Error refreshing token:', data.message);
+            return data.message;
+        }
+    } catch (error) {
+        console.error('Error refreshing token:', error);
+        return error;
+    }
+}
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [loading, setLoading] = useState(true);
@@ -35,7 +65,15 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
                     if (response.status === 401) {
                         localStorage.removeItem('token');
                     }
-                    setIsAuthenticated(false);
+                    console.log('Token invalid. Checking refresh token...');
+                    const refreshTokenError = await CheckRefreshToken();
+                    if (!refreshTokenError) {
+                        console.log('Refresh token successful');
+                        setIsAuthenticated(true);
+                    } else {
+                        console.error('Error refreshing token:', refreshTokenError);
+                        setIsAuthenticated(false);
+                    }
                 }
             } catch (error) {
                 console.error('Error checking token:', error);
